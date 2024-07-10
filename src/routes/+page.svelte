bfe43b43424b5eb5fe3695d3ed87e5105310e61a
TheJmqn <jman4461@gmail.com>
2024-07-09 23:14:26 -0400

    Conference listing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script>
 	import axios from "axios";
-    import { Button, Range, Label } from 'flowbite-svelte';
+    import { Button, Checkbox, Dropdown, DropdownItem, Range, Label } from 'flowbite-svelte';
 
+    const conferences = [
+        {name: "ACC", code: "ACC", type: "P4", checked: false},
+        {name: "Big 12", code: "B12", type: "P4", checked: false},
+        {name: "Big Ten", code: "B1G", type: "P4", checked: false},
+        {name: "SEC", code: "SEC", type: "P4", checked: false},
+        {name: "PAC-12", code: "PAC", type: "FBS", checked: false},
+        {name: "American Athletic", code: "AAC", type: "FBS", checked: false},
+        {name: "Conference USA", code: "CUSA", type: "FBS", checked: false},
+        {name: "Mid-American", code: "MAC", type: "FBS", checked: false},
+        {name: "Mountain West", code: "MWC", type: "FBS", checked: false},
+        {name: "Sun Belt", code: "SBC", type: "FBS", checked: false},
+        {name: "FBS Independents", code: "Ind", type: "FBS", checked: false}
+    ];
     let conference = "";
     let status = {
         text: "",
@@ -44,6 +57,7 @@
     let response = [];
 
     function callBackend() {
+        console.log(conferences);
         if (!conference) {
             status = {text: "Please enter a code.", positive: false};
             return;
@@ -69,17 +83,24 @@
 </script>
 
 <h1>CFB Auto Power Rankings</h1>
-<input type="text" bind:value={conference}/>
-<p>{conference}</p>
+<button class="bg-gray-600">Dropdown checkbox</button>
+<Dropdown class="w-44 p-3 space-y-3 text-sm bg-gray-600">
+    {#each conferences as conference, i}
+        <li id={conference.code} class="rounded p-2 hover:bg-gray-100 dark:hover:bg-gray-600">
+            <Checkbox on:change={() => conferences[i].checked = !conferences[i].checked}>{conference.name}</Checkbox>
+        </li>
+    {/each}
+</Dropdown>
+
 <p style="color: {status.positive ? 'green' : 'red'}">{status.text}</p>
 <button on:click={callBackend}>Click to call</button>
 
 {#if response}
-    <ol>
+    <ul class="max-w-md space-y-1 list-decimal list-inside">
     {#each response as team}
-        <li>{team.Team}</li>
+        <li>{team.Team} (Score: {team.Score})</li>
     {/each}
-    </ol>
+    </ul>
 {:else}
     <p>Waiting for response.</p>
 {/if}
@@ -87,7 +108,7 @@
 <ul>
     {#each new Map(Object.entries(teamStats)) as stat}
     <Label>{stat[0]}</Label>
-    <Range id={stat} min="0" max="100" bind:value={teamStats[stat[0]]}/>    
+    <Range id={stat[0]} min="0" max="100" bind:value={teamStats[stat[0]]}/>    
     <li>{stat[1]}</li>
     {/each}
 </ul>
